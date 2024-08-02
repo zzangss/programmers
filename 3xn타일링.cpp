102a2b06b35f46bbc49357e248a71e5ac2eac96f
zzangss <164142490+zzangss@users.noreply.github.com>
2024-08-03 05:20:59 +0900

    Update 3xn타일링.cpp

--- 3xn타일링.cpp
+++ 3xn타일링.cpp
@@ -1,22 +1,23 @@
 #include<iostream>
 #include <string>
 #include <vector>
+#define MOD 1000000007
 
 using namespace std;
 
-int solution(int n) {
-    int arr[5001] = { 0, };
+long long solution(int n) {
+    long long arr[5001] = { 0, };
 
     arr[2] = 3;
     for (int i = 4; i <= n; i += 2) {
-        arr[i] += arr[i - 2] * 3 + 2;
+        arr[i] += (arr[i - 2] * 3 + 2)%MOD;
 
         for (int j = i - 4; j > 0; j--) {
-            arr[i] += arr[j] * 2;
+            arr[i] += (arr[j] * 2)%MOD;
         }
     }
 
-    return arr[n];
+    return arr[n]%MOD;
 }
 
 int main() {
